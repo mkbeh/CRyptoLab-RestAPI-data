fe6a9d076b75df22e43946fc893987cfcd562ef9
noragami <mkbehforever@gmail.com>
2018-09-21 20:48:49 +0300

    Add method for checking api status.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import utils
 from pymongodb.pymongodb import MongoDB
 
 
+@hug.get('/api')
+def api():
+    return {'status': 'ok'}
+
+
 @hug.get('/ico/{parser_name}&cat={cat}&limit={limit}&skip={skip}')
 def ico(parser_name: str, cat: str, limit: int, skip: int):
     mongo = MongoDB(parser_name)
