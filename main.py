2f08169cbcf5d7962e97e1cf49a3e40d656f6fd2
noragami <mkbehforever@gmail.com>
2018-09-28 21:21:58 +0300

    Add method for return bits media news.

--- main.py
+++ main.py
@@ -13,6 +13,26 @@ def api():
     return {'status': 'ok'}
 
 
+@hug.get('/news/{parser_name}&limit={limit}&skip={skip}')
+def news(parser_name: str, limit: int, skip: int):
+    mongo = MongoDB(parser_name)
+
+    # Get day obj and date.
+    from datetime import datetime
+    day = datetime.today().day
+    date = datetime.today().strftime('{}.%m.%Y')
+
+    # Find documents.
+    documents = mongo.find({'date': date.format(day)}, 'news', limit, skip)
+
+    if not documents:
+        documents = mongo.find({'date': date.format(day - 1)}, 'news', limit, skip)
+
+    mongo.finish()
+
+    return json.dumps(list(map(functools.partial(utils.del_dict_item, key_name='_id'), documents)))
+
+
 @hug.get('/ico/{parser_name}&cat={cat}&limit={limit}&skip={skip}')
 def ico(parser_name: str, cat: str, limit: int, skip: int):
     mongo = MongoDB(parser_name)
